1b9d0371d19ea0bae01f4d88afe09c1bf6ef1348
Henk Nicolai <PenguinF@users.noreply.github.com>
2019-07-30 21:40:18 +0200

    Add 'choose' function. Example: choose "xy" 3

--- combinatorics.hs
+++ combinatorics.hs
@@ -1,7 +1,8 @@
 module Eutherion.Combinatorics (
 
        binom,
-       multinom
+       multinom,
+       choose
 
        ) where
 
@@ -26,3 +27,26 @@ multinom n ks =
     case ks of
         []   -> 1
         k:ks -> multinom (n - k) ks * binom n k
+
+-- Enumerates all combinations to choose n items out of a set of k distinct elements.
+-- Example: to enumerate all ways of choosing 3 characters out of 'x' and 'y':
+--
+-- > choose "xy" 3
+-- [(1,[(3,'x'),(0,'y')]),
+--  (3,[(2,'x'),(1,'y')]),
+--  (3,[(1,'x'),(2,'y')]),
+--  (1,[(0,'x'),(3,'y')])]
+--
+-- I.e. 1 way to choose 3 'x', 3 ways of choosing either 2 'x' and 1 'y' or 1 'x' and 2 'y', and 1 way to choose 3 'y'.
+choose :: [a] -> Integer -> [(Integer, [(Integer, a)])]
+choose items n =
+    let k = toInteger $ length items
+    in  [(multinom n distinctSum, zip distinctSum items) | distinctSum <- reverse $ genDistinctSums n k]
+    where
+        -- Generates all distinct combinations of k integers which add up to a total of n.
+        genDistinctSums :: (Eq a, Enum a, Num a, Eq b, Num b) => a -> b -> [[a]]
+        genDistinctSums n k =
+            case (n, k) of
+                (0, 0) -> [[]]  -- Sum matches total exactly, accept, so yield an empty (valid) solution.
+                (_, 0) -> []    -- Sum does not match total, reject, so yield nothing at all.
+                (n, k) -> [i : xs | i <- [0..n], xs <- genDistinctSums (n - i) (k - 1)]
