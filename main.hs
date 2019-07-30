0d1e7339fe13a88a015c732e1354c72aa8d10729
Henk Nicolai <PenguinF@users.noreply.github.com>
2019-07-30 22:55:17 +0200

    Add example Pólya group for square boards with pieces (chess, tic-tac-toe, checkers, ...).

--- main.hs
+++ main.hs
@@ -7,7 +7,9 @@ import Eutherion.Combinatorics
 import Eutherion.Polya
 
 
--- Example: graphs on 1 or more points.
+
+
+-- Example I: graphs on 1 or more points.
 
 -- Usage:
 -- -- Get the number of generated permutation functions which map an edge to another, for graphs with 4 vertices:
@@ -56,3 +58,50 @@ graphPolyaGroup n =
     makePolyaGroup (enumUndirectedNonReflexiveGraphEdges ug) (enumPermuteGraphEdgeMappings ug)
     where
         ug = undirectedGraph 0 (n - 1)
+
+
+
+
+-- Example II: square boards of size 1 and greater.
+
+-- -- Show the Cayley table of the Pólya group for Tic-Tac-Toe boards:
+-- > cayleyTable $ sqBoardPolyaGroup 3
+
+data SquareBoard a = SquareBoard a
+
+squareBoard :: (Ord a, Num a) => a -> SquareBoard a
+squareBoard size = assert (size > 0) $ SquareBoard size
+
+data SquareBoardCoordinate a = SquareBoardCoordinate a a deriving (Show, Eq)
+
+coordinate :: (Ord a, Enum a) => SquareBoard a -> a -> a -> SquareBoardCoordinate a
+coordinate (SquareBoard size) x y = assert (toEnum 0 <= x && x < size && toEnum 0 <= y && y < size) $ SquareBoardCoordinate x y
+
+-- All 8 members of the square board symmetry group - given a board size.
+symmetryId    :: p -> a -> a
+vFlip         :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+hFlip         :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+slashFlip     :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+backSlashFlip :: p -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+rotate90      :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+rotate180     :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+rotate270     :: Num a => a -> SquareBoardCoordinate a -> SquareBoardCoordinate a
+
+-- (0, 0) is assumed to be at the top-left corner of the board.
+symmetryId    _                             = id
+vFlip         n (SquareBoardCoordinate x y) = SquareBoardCoordinate (n - 1 - x) y
+hFlip         n (SquareBoardCoordinate x y) = SquareBoardCoordinate x           (n - 1 - y)
+slashFlip     n (SquareBoardCoordinate x y) = SquareBoardCoordinate (n - 1 - y) (n - 1 - x)
+backSlashFlip _ (SquareBoardCoordinate x y) = SquareBoardCoordinate y           x
+rotate90      n (SquareBoardCoordinate x y) = SquareBoardCoordinate y           (n - 1 - x)
+rotate180     n (SquareBoardCoordinate x y) = SquareBoardCoordinate (n - 1 - x) (n - 1 - y)
+rotate270     n (SquareBoardCoordinate x y) = SquareBoardCoordinate (n - 1 - y) x
+
+enumBoardSymmetryOperations :: (Ord a, Num a) => SquareBoard a -> [SquareBoardCoordinate a -> SquareBoardCoordinate a]
+enumBoardSymmetryOperations (SquareBoard n)
+    | n > 1     = [symmetryId n, vFlip n, hFlip n, slashFlip n, backSlashFlip n, rotate90 n, rotate180 n, rotate270 n]
+    | otherwise = [symmetryId n]  -- trivial group
+
+sqBoardPolyaGroup :: (Ord a, Enum a, Num a) => a -> PolyaGroup (SquareBoardCoordinate a)
+sqBoardPolyaGroup n =
+    makePolyaGroup [SquareBoardCoordinate x y | x <- [0..n - 1], y <- [0..n - 1]] (enumBoardSymmetryOperations (squareBoard n))
