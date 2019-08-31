2c83c2c3c687c5565a8be750c2886e1229872f24
Henk Nicolai <PenguinF@users.noreply.github.com>
2019-09-01 00:24:08 +0200

    Add characteristic function.

--- polya.hs
+++ polya.hs
@@ -2,7 +2,8 @@ module Eutherion.Polya (
 
        PolyaGroup,
        makePolyaGroup,
-       cayleyTable
+       cayleyTable,
+       characteristic
 
        ) where
 
@@ -11,6 +12,7 @@ import Data.List
 
 import Eutherion.Utilities
 import Eutherion.Combinatorics
+import Eutherion.Polynomial
 
 -- Encapsulates a list of elements, and a list of symmetry operations acting on those elements.
 data PolyaGroup a = PolyaGroup [a] [a -> a]
@@ -63,3 +65,46 @@ cayleyTable (PolyaGroup slots fns) =
                     case elemIndex slots secondFnApplied of
                         Just x  -> x
                         Nothing -> error ("Function " ++ show i ++ " has no inverse.")
+
+
+-- Returns the characteristic polynomial for a Pólya group and a number of choices represented by characters.
+characteristic :: (Eq a, Ord b) => PolyaGroup a -> [b] -> (Integer, Polynomial Integer b)
+characteristic (PolyaGroup slots symmetries) cs =
+    (toInteger $ length symmetries, genExpression cs $ choiceFunction orbitLengths)
+    where
+        -- For sqBoardPolyaGroup 3, orbitLengths has this value:
+        -- [[1,1,1,1,1,1,1,1,1],[2,2,2,1,1,1,2,2,2],[2,1,2,2,1,2,2,1,2],[2,2,1,2,1,2,1,2,2],[1,2,2,2,1,2,2,2,1],[4,4,4,4,1,4,4,4,4],[2,2,2,2,1,2,2,2,2],[4,4,4,4,1,4,4,4,4]]
+        orbitLengths = [[(+1) $ length $ takeWhile (/= slot) $ tail $ iterate symmetry slot | slot <- slots] | symmetry <- symmetries]
+
+        choiceFunction :: [[Int]] -> [[Int]]
+        choiceFunction os = map removeSharedOrbits os
+
+        removeSharedOrbits :: [Int] -> [Int]
+        removeSharedOrbits os = removeSharedOrbits' (sort os)
+            where
+                removeSharedOrbits' os =
+                    case os of
+                        []   -> []
+                        o:os -> o : removeSharedOrbits' (drop (o - 1) os)
+
+        genExpression :: (Integral a, Ord b) => [b] -> [[a]] -> Polynomial Integer b
+        genExpression cs orbitGroups = addPoly $ groupAndSort valuesForAllOrbits compare orbitGroups
+            where
+                -- Expression to choose one element out of cs for 'orbitLength' slots simultaneously (i.e. they are all the same).
+                singleChoice cs = addPoly [makeVar c | c <- cs]
+                multipleChoice orbitLength cs = addPoly [expPoly (makeVar c) (toInteger orbitLength) | c <- cs]
+                selectOneValue orbitLength = case orbitLength of
+                    1           -> singleChoice
+                    orbitLength -> multipleChoice orbitLength
+
+                -- Expression to choose values for groups of slots with the same choice expression.
+                selectIndependentValues slotGroupCount choiceExpression = case slotGroupCount of
+                    1              -> choiceExpression
+                    slotGroupCount -> expPoly choiceExpression (toInteger slotGroupCount)
+
+                -- Choose independent values for a group of slots with the same orbit length.
+                multipleIndependentValues orbitLength grp = [selectIndependentValues (length grp + 1) (selectOneValue orbitLength cs)]
+
+                -- Choose independent values for all orbits of one symmetry operation,
+                -- then multiply by the number of symmetries which share the same set of orbit lengths.
+                valuesForAllOrbits orbits orbitGroup = [multPoly (makeConst (toInteger (1 + length orbitGroup)) : (groupAndSort multipleIndependentValues compare orbits))]
