b57117511eee45e7be1de0750313c71a55d477a2
Henk Nicolai <PenguinF@users.noreply.github.com>
2019-09-19 20:21:31 +0200

    Comment on 'assertFnClosed' and 'applied' functions.

--- polya.hs
+++ polya.hs
@@ -43,8 +43,10 @@ cayleyTable (PolyaGroup slots namedFns) =
 
         listToZeroIndexedArray xs = listArray (0, length xs - 1) xs
 
+        -- Applies each symmetry operation on each element in 'slots'.
         applied = [assertClosed slots (zip [0..] [fn slot | slot <- slots]) | fn <- fns]
             where
+                -- Checks if each function maps each slot onto another slot from the list.
                 assertClosed slots slots' =
                     let resultInSlots = [(i, elem x slots) | (i, x) <- slots']
                     in  case all snd resultInSlots of
